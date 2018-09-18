1b0bf0ff7833ba8534ce3e8a8d58d20aea069aae
Nathan Douds <nathan.douds@catchco.com>
2018-09-18 07:23:41 -0400

    fix bad json loop handling

--- Njd.Bakery.Api2/Startup.cs
+++ Njd.Bakery.Api2/Startup.cs
@@ -21,7 +21,11 @@ namespace Njd.Bakery.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(
+                    options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                );
 
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
