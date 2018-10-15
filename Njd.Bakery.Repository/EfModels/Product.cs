7e59f742113e5dbf0627fc480af4895ea198d6b9
Nathan Douds <nathan.douds@catchco.com>
2018-10-15 07:10:26 -0400

    Move fields around

--- Njd.Bakery.Repository/EfModels/Product.cs
+++ Njd.Bakery.Repository/EfModels/Product.cs
@@ -7,25 +7,27 @@ namespace Njd.Bakery.Repository.EfModels
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public string Name { get; set; }
         // this would probably be the unique public facing identifier
         public string Sku { get; set; }
-        // the goal here is that default will be the highest level, but it can have nested alternates for allergies
-        public bool IsDefaultProduct => ParentId == null;
-        public bool GlutenFree { get; set; }
-        public bool CanBeGlutenFree { get; set; }
-        public bool GrainFree { get; set; }
-        public bool CanBeGrainFree { get; set; }
+
         public bool DairyFree { get; set; }
         public bool CanBeDairyFree { get; set; }
         public bool EggFree { get; set; }
         public bool CanBeEggFree { get; set; }
-        public bool RefinedSugarFree { get; set; }
-        public bool CanBeRefinedSugarFree { get; set; }
+        public bool GlutenFree { get; set; }
+        public bool CanBeGlutenFree { get; set; }
+        public bool GrainFree { get; set; }
+        public bool CanBeGrainFree { get; set; }
         public bool NutFree { get; set; }
         public bool CanBeNutFree { get; set; }
+        public bool RefinedSugarFree { get; set; }
+        public bool CanBeRefinedSugarFree { get; set; }
         public bool Vegan { get; set; }
         public bool CanBeVegan { get; set; }
+
+
         public int DefaultNumberOfServings { get; set; }
         public decimal TotalBatchCalories { get; set; }
         public decimal TotalBatchFat { get; set; }
@@ -33,6 +35,9 @@ namespace Njd.Bakery.Repository.EfModels
         public decimal TotalBatchFiber { get; set; }
         public decimal TotalBatchSugar { get; set; }
         public decimal TotalBatchProtein { get; set; }
+
+        // the goal here is that default will be the highest level, but it can have nested alternates for allergies
+        public bool IsDefaultProduct => ParentId == null;
 
         public Product Parent { get; set; }
         public int? ParentId { get; set; }
